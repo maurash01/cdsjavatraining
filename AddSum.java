fcc56756416f90608b6ce1df95945127e6bd5bb8
maurash01 <ashumaurya023@gmail.com>
2024-07-22 15:28:00 +0530

    Add tow no. with different datatypes

---
+++ AddSum.java
@@ -0,0 +1,8 @@
+public class AddSum {
+    public static void main(String[] args) {
+        int a =  5;
+        float b =  7.5f ;
+        System.out.println("The Sum is : " + a+b );
+    }
+    
+}
