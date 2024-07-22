324b0b4c2fbb17f896c477bd554ab928aa313331
maurash01 <ashumaurya023@gmail.com>
2024-07-22 15:34:08 +0530

    Add two no. with different datatypes

--- AddSum.java
+++ AddSum.java
@@ -2,7 +2,7 @@ public class AddSum {
     public static void main(String[] args) {
         int a =  5;
         float b =  7.5f ;
-        System.out.println("The Sum is : " + a+b );
+        System.out.println("The Sum is : " + (a+b) );
     }
     
 }
